0388aca5139128e07207c4013e2eb7cd8bff4e34
= <emilianenko1205@gmail.com>
2024-10-18 07:14:44 -0500

    Proyecto de AppAgregación completado

--- EjercicioAgregacion/src/clasesagregacion/Factura.java
+++ EjercicioAgregacion/src/clasesagregacion/Factura.java
@@ -1,6 +1,7 @@
 package clasesagregacion;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Factura extends Comprobante {
     private float total;
@@ -38,8 +39,29 @@ public class Factura extends Comprobante {
 
     public void agregarProducto(Producto p) {
         mProducto.add(p);
-        setTotal(getTotal() + p.getPrecio());
-        
+        setTotal(getTotal() + p.getPrecio()); 
     }
+
+    public void mostrarProducto() {
+        Iterator<Producto> iter = mProducto.iterator();
+        while(iter.hasNext()) {
+            Producto p = iter.next();
+            System.out.printf("Codigo: %d Descripcion: %s Precio: %5.2f\n", p.getCodigo(), p.getDescripcion(), p.getPrecio());
+        }
+    }
+
+    public void mostrar() {
+        System.out.printf("Tipo: %c Numero: %d Fecha: %d%d%d\n", getTipo(), getNumero(), getFecha().getDia(), getFecha().getMes(), getFecha().getanio());
+
+        System.out.printf("Cliente: \n");
+
+        System.out.printf("Codigo: %d RazonSocial: %s\n", mCliente.getCodigo(), mCliente.getRazonSocial());
+
+        System.out.printf("Productos: \n");
+
+        mostrarProducto();
+
+        System.out.printf("Total: %5.2f \n", getTotal());
+    }    
 }
 
